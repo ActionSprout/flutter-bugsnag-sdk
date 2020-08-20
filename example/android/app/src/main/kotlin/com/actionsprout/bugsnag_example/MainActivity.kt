6d2908560bf3e8fb62be5fc620b2ce6afb5e3bc1
Michael Schoonmaker <hello@schoonology.com>
2020-08-20 13:08:49 -0700

    Initial Flutter boilerplate.

---
+++ example/android/app/src/main/kotlin/com/actionsprout/bugsnag_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.actionsprout.bugsnag_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ ios/Classes/BugsnagPlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface BugsnagPlugin : NSObject<FlutterPlugin>
+@end
